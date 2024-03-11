7ba6a729bf90164dc2d1b8e27d74b2ab0bee5f0d
yosuket323 <yosuket@leapcom365.com>
2024-03-11 15:52:01 +0900

    add webmanifest

--- pwa.js
+++ pwa.js
@@ -1,3 +1,4 @@
+// 通知の実装
 window.addEventListener('load', () => {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
@@ -52,3 +53,33 @@ function post_message(str) {
         registration.active.postMessage(str);
     });
 }
+
+// インストールボタンの実装
+let deferredPrompt;
+const addBtn = document.querySelector(".add-button");
+addBtn.style.display = "none";
+
+window.addEventListener("beforeinstallprompt", (e) => {
+    // Chrome 67以前のバージョンでプロンプトが自動的に表示されないようにする
+    e.preventDefault();
+    // 後で発生させることができるように、イベントを隠しておく。
+    deferredPrompt = e;
+    // ホーム画面に内側へ追加できることをユーザーに通知する UI の更新
+    addBtn.style.display = "block";
+    
+    addBtn.addEventListener("click", (e) => {
+        // A2HS ボタンを表示するユーザーインターフェイスを非表示にします。
+        addBtn.style.display = "none";
+        // プロンプトを表示
+        deferredPrompt.prompt();
+        // ユーザーがプロンプトに応答するのを待つ
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === "accepted") {
+                console.log("ユーザーが A2HS プロンプトを受け入れました。");
+            } else {
+                console.log("ユーザーは A2HS のプロンプトを拒否しました。");
+            }
+            deferredPrompt = null;
+        });
+    });
+});
