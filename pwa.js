279432a9f8735c3c50fa7a8db482cd64b05c6a04
yosuket323 <yosuket@leapcom365.com>
2024-03-11 18:39:06 +0900

    プッシュ通知を実装

--- pwa.js
+++ pwa.js
@@ -1,5 +1,6 @@
-// 通知の実装
+// サービスワーカーの起動
 window.addEventListener('load', () => {
+    // service worker を登録し起動する
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker
             .register("sw.js")
@@ -8,6 +9,7 @@ window.addEventListener('load', () => {
     }
 });
 
+// プッシュ通知の許可、テスト送信
 document.getElementById("register").addEventListener('click', onClickRegister);
 document.getElementById("send").addEventListener("click", onClickSend);
 
@@ -25,7 +27,6 @@ function onClickRegister() {
         }
     });
 }
-
 function onClickSend() {
     if (!check_notification()) {
         return;
@@ -39,7 +40,6 @@ function onClickSend() {
         alert("通知が許可されていません");
     }
 }
-
 function check_notification() {
     if (!('Notification') in window) {
         alert("このブラウザはプッシュ通知に非対応です");
@@ -47,7 +47,6 @@ function check_notification() {
     }
     return true;
 }
-
 function post_message(str) {
     navigator.serviceWorker.ready.then(registration => {
         registration.active.postMessage(str);
@@ -57,15 +56,60 @@ function post_message(str) {
 // インストールボタンの実装
 registerInstallAppEvent(document.getElementById("add-button"));
 function registerInstallAppEvent(elem) {
+    console.log(elem)
     function installApp() {
-      if(elem.promptEvent){
         console.log("install button clicked.");
-        elem.promptEvent.prompt();
-        elem.promptEvent.userChoice.then(function(choice){
-            elem.style.display = "none";
-            elem.promptEvent = null;
+        if(elem.promptEvent){
+            elem.promptEvent.prompt();
+            elem.promptEvent.userChoice.then(function(choice){
+                elem.style.display = "none";
+                elem.promptEvent = null;
         });
       }
     }
     elem.addEventListener("click", installApp);
+}
+
+// ブラウザのプッシュサービスに登録する
+function notification_subscribe() {
+    const pushServerPublicKey = 'BDHm_FQjWwXjcMi7f0T3B37PTUTk8F-SQNoDMvWaUzANa34Bvka9xYdpsH5xUHocQ-mkQDhYWXrn6PnrStaGlms';
+    return navigator.serviceWorker.ready.then(
+        (serviceWorker) => {
+        // subscribe and return the subscription
+        return serviceWorker.pushManager
+        .subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: pushServerPublicKey
+        }).then(
+        (subscription) => {
+            // TODO: send subscription.endpoint to server
+            console.log ("プッシュサービス 登録成功")
+            console.log (subscription.toJSON())
+            _showWebPushCommand (subscription.toJSON());
+            return subscription;
+        },
+        (error) => {
+            console.log("プッシュサービス 登録失敗");
+            console.log(error);
+        });
+        }
+    );
+}
+function _showWebPushCommand(subscriptionInfo)
+{
+    const command = [
+        "auth = mailto:yosuket@leapcom365.com",
+        "公開鍵 = BDHm_FQjWwXjcMi7f0T3B37PTUTk8F-SQNoDMvWaUzANa34Bvka9xYdpsH5xUHocQ-mkQDhYWXrn6PnrStaGlms",
+        "秘密鍵 = ZEnkVXMttEU1MuOvENWZVL7dMYlnVNupxRPezGkPxDQ",
+        "endpoint = " + subscriptionInfo.endpoint,
+        "p256dh = " + subscriptionInfo.keys.p256dh,
+        "auth = " + subscriptionInfo.keys.auth,
+    ];
+
+    var text = ("この内容で push.js の subscribers に登録してください:");
+    text += command.join(" ");
+
+    console.log(text);
+
+    //document.getElementById("")
 }
--- sw.js
+++ sw.js
@@ -6,3 +6,18 @@ self.addEventListener('message', function (event) {
     console.log(event.data);
     self.registration.showNotification(event.data);
 })
+
+function receivePushNotification(event) {
+    const options = {
+      data: "データ",
+      body: "内容",
+    };
+    event.waitUntil(this.registration.showNotification("タイトル", options));
+}
+this.addEventListener("push", receivePushNotification);
+  
+function openPushNotification(event) {
+    event.notification.close();
+    event.waitUntil(clients.openWindow(event.notification.data));
+}
+this.addEventListener("notificationclick", openPushNotification);
